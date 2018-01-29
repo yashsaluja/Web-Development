91b5868fa52738b580c064e6923f6ffc7b1235c7
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-29 18:45:43 +0530

    javascript

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-var userName =prompt("What's my name?");
-alert("Nice " + userName);
-console.log("Great to meet you," + username);
+var firstName =prompt("What is your first name?");
+var lastName =prompt("What is your last name?");
+var age =prompt("What is your age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old ");
