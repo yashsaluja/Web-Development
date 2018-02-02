81d5600e9f7a4f409aacec784b79380d8ce417ca
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-02 19:05:27 +0530

    annoy o matic

--- annoy.js
+++ annoy.js
@@ -1,8 +1,20 @@
+
+//var answer = prompt("Are we there bhencho?");
+
+//while(answer !== "yes")
+//{
+//	var answer = prompt("Are we there bhencho?");
+//}
+
+//alert("We made it!!");
+
+
 var answer = prompt("Are we there bhencho?");
 
-while(answer !== "yes")
+while(answer.indexOf("yes") == -1)
 {
 	var answer = prompt("Are we there bhencho?");
 }
 
 alert("We made it!!");
+
