5df3f3ac97ef0e22e889f8ab9409506f59147c43
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-03-17 16:50:36 +0530

    ToDo List

--- list.js
+++ list.js
@@ -1,19 +1,31 @@
-var todos =["Dhoni"];
+var todos =["FIFA
 
 var input = prompt("What would you like to do?");
 
 while(input !== "quit")
 {
 	if(input === "list")
-	{
-		console.log(todos);
+	{ 
+		console.log("**********");
+		todos.forEach(function(todo, i)
+		{
+			console.log(i + ": " + todo);
+		});
+		console.log("*********");
 
 	}
+
 	else if(input === "new")
 	{
 		var newtodo =prompt("Enter new todo");
 		todos.push(newtodo);
 	}
+	else if(input === "delete")
+	{
+		var index = prompt("Enter index of todo to delete");
+		todos.splice(index,1);
+	}
+
 	input = prompt("what would you like to do?");
 }  
 console.log("quit");
